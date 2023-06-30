6d97cda3c0d0a194e3280a3be4a2fd4dcb2e0e1f
DmitMark <dmmarkushevski@gmail.com>
2023-06-30 10:33:49 +0300

    add negative tests

--- settings.py
+++ settings.py
@@ -4,3 +4,5 @@ valid_password = '123'
 
 invalid_email = "nukdymailtoplus"
 invalid_password = '567'
+
+invalid_key = ({'key': 'fca407ea94c5c23772efcaf731e9a70b267a33f311bbc4292ca9fb81'})
--- tests/test_pet_friends.py
+++ tests/test_pet_friends.py
@@ -1,13 +1,15 @@
 from api import PetFriends
-from settings import valid_email, valid_password, invalid_email, invalid_password
+from settings import valid_email, valid_password, invalid_email, invalid_password, invalid_key
 import os
 
 pf = PetFriends()
 def test_get_api_key_for_valid_user(email = valid_email, password = valid_password):
     status, result = pf.get_api_key(email, password)
 
+    print(result)
     assert status == 200
     assert 'key' in result
+
 
 def test_get_api_key_for_invalid_user(email = invalid_email, password = invalid_password):
     status, result = pf.get_api_key(email, password)
@@ -31,6 +33,14 @@ def test_get_all_pets_with_valid_key(filter=''):
     assert status == 200
     assert len(result['pets']) > 0
 
+def test_get_all_pets_with_invalid_key(filter=''):
+
+    auth_key = invalid_key
+    status, result = pf.get_list_of_pets(auth_key, filter)
+
+    assert status == 403
+
+
 def test_get_my_pets_with_valid_key(filter='my_pets'):
 
     _, auth_key = pf.get_api_key(valid_email, valid_password)
@@ -53,7 +63,32 @@ def test_add_new_pet_with_valid_data(name='Jack', animal_type='dog',
     assert result['animal_type'] == animal_type
     assert result['age'] == age
 
-def test_add_new_pet_with_invalid_data(name='Bobik', animal_type='двортерьер',
+def test_add_new_pet_with_invalid_data_photo(name='Jack', animal_type='dog',
+                                     age='4', pet_photo='images/dog3.ico'):
+
+    pet_photo = os.path.join(os.path.dirname(__file__), pet_photo)
+
+    _, auth_key = pf.get_api_key(valid_email, valid_password)
+
+    status, result = pf.add_new_pet(auth_key, name, animal_type, age, pet_photo)
+
+    assert status == 400
+    # assert result['name'] == name
+    # assert result['animal_type'] == animal_type
+    # assert result['age'] == age
+
+def test_add_new_pet_with_invalid_key(name='Jack', animal_type='dog',
+                                     age='4', pet_photo='images/dog1.jpg'):
+
+    pet_photo = os.path.join(os.path.dirname(__file__), pet_photo)
+
+    auth_key = invalid_key
+
+    status, result = pf.add_new_pet(auth_key, name, animal_type, age, pet_photo)
+
+    assert status == 403
+
+def test_add_new_pet_with_invalid_data_age(name='Bobik', animal_type='двортерьер',
                                      age='four', pet_photo='images/dog1.jpg'):
 
     pet_photo = os.path.join(os.path.dirname(__file__), pet_photo)
@@ -77,6 +112,21 @@ def test_successful_delete_self_pet():
     status, _ = pf.delete_pet(auth_key, pet_id)
 
     assert status == 200
+    assert pet_id not in my_pets.values()
+
+def test_failed_delete_self_pet_with_invaled_key():
+    _, auth_key = pf.get_api_key(valid_email, valid_password)
+    _, my_pets = pf.get_list_of_pets(auth_key, "my_pets")
+
+    if len(my_pets['pets']) == 0:
+        pf.add_new_pet(auth_key, "Jack", "doge", "3", "images/dog1.jpg")
+        _, my_pets = pf.get_list_of_pets(auth_key, "my_pets")
+
+    pet_id = my_pets['pets'][0]['id']
+    auth_key = invalid_key
+    status, _ = pf.delete_pet(auth_key, pet_id)
+
+    assert status == 403
     assert pet_id not in my_pets.values()
 
 def test_successful_update_self_pet_info(name='Tom', animal_type='cat', age='5'):
